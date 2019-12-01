77714437fa9f533c5f38fbc8e20dd084332e2e64
alejandro quesada tinco <tqaw19@gmail.com>
2019-12-01 13:10:37 -0500

    first commit

---
+++ WebAPIService/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPIService.Models
+{
+    public class Product
+    {
+        //comment
+
+        public int ProductID { get; set; }
+
+        public string ProductName { get; set; }
+
+        public Nullable<int> Quantity { get; set; }
+
+        public Nullable<int> Price { get; set; }
+
+    }
+}
